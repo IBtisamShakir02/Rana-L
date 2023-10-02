d8e81c4243ad963b4bc58665d6f07b4e4f509f4a
Ibtisam <ibtisam.route@gmail.com>
2023-10-03 00:07:49 +0500

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data";
 
-function App() {
+const App = () => {
+  const [articles, setArticles] = useState(data);
+  const [sortOrder, setSortOrder] = useState("upvotes"); // Default sort order
+
+  const handleSort = (sortBy) => {
+    if (sortBy === "upvotes") {
+      setArticles([...articles].sort((a, b) => b.upvotes - a.upvotes));
+    } else if (sortBy === "date") {
+      setArticles(
+        [...articles].sort((a, b) => new Date(b.date) - new Date(a.date))
+      );
+    }
+    setSortOrder(sortBy);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div style={{ marginTop: 50 }}>
+        <table cellPadding={5} cellSpacing={5} width={"70%"}>
+          <thead>
+            <tr className="row">
+              <th>Title</th>
+              <th>Upvotes</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {articles.map((article, index) => (
+              <tr className="row" key={index}>
+                <td className="txt">{article.title}</td>
+                <td className="txt">{article.upvotes}</td>
+                <td className="txt">{article.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <div className="btn">
+        <button onClick={() => handleSort("upvotes")}>Most Upvoted</button>
+        <button onClick={() => handleSort("date")}>Most Recent</button>
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,29 @@
+const data  = [
+{
+    title: "The Future of Artificial Intelligence: Trends and Possibilities",
+    upvotes:20,
+    date:"2023-09-10"
+},
+{
+    title:"10 JavaScript Libraries Every Developer Should Know" ,
+    upvotes: 25,
+    date:"2023-08-12"
+},
+{
+    title: "Mastering Responsive Web Design: Tips and Techniques",
+    upvotes:10,
+    date:"2023-09-15"
+},
+{
+    title: "The Rise of Progressive Web Apps: Benefits and Best Practices" ,
+    upvotes:50,
+    date:"2023-02-10"
+},
+{
+    title: "Demystifying Blockchain Technology: A Comprehensive Guide",
+    upvotes:90,
+    date:"2023-06-20"
+}
+
+]
+export default data;
